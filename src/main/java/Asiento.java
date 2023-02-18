47d127cb6f9930b9b75eddf9e142558a8262891c
efrain <efrain200414@gmail.com>
2023-02-18 07:05:20 -0500

    faltaron unos cuantos archivos :P creo que todos porque use un ataja sin hacer un paso antes

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,16 @@
+// package test
+public class Asiento {
+    public String color;
+    public int precio;
+    public int registro;
+
+    public void cambiarColor(String color){
+        String[] allowColor = {"rojo","verde","amarillo","negro","blanco"}; 
+
+        for (int i=0;i<allowColor.length;i++){
+            if (color.equalsIgnoreCase(allowColor[i])){
+                this.color = color;
+            }
+        }
+    } 
+}
---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+package test;
+public class Auto {
+    public String modelo;
+    public int precio;
+    public Asiento[] asientos;
+    public String marca;
+    public Motor motor;
+    public int registro;
+    public static int cantidadCreados;
+    
+    public int cantidadAsientos(){
+        int canAsientos = 0;
+        for(int i = 0;i<asientos.length;i++){
+            if (asientos[i] != null){
+                canAsientos++;
+            }
+        }
+        return canAsientos;
+
+    }
+
+    public String verificarIntegridad(){
+        if (motor.registro != this.registro){
+            return "Las piezas no son originales";
+        }
+        
+        for(int i = 0;i<asientos.length;i++){
+            if ((asientos[i] != null) && (this.registro != asientos[i].registro)){
+                return "Las piezas no son originales";
+            }
+        }
+
+        return "Auto original";
+    }
+}
